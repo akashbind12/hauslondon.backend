cf1648f860ca5ddd1503535a12f12c35627d0c0e
akashbind12 <97519781+akashbind12@users.noreply.github.com>
2022-06-04 23:48:06 +0530

    final

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const data = require("./db.js")
+
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(data);
+const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
@@ -10,3 +10,4 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
